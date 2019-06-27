b0d920e407403b7c693fcaffc135e517c30f6d0a
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-06-26 22:49:33 -0300

    Verificacoes avancadas

--- src/main/java/br/com/wcanquino/rest/UserJsonTest.java
+++ src/main/java/br/com/wcanquino/rest/UserJsonTest.java
@@ -1,6 +1,10 @@
 package br.com.wcanquino.rest;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.arrayContaining;
+import static org.hamcrest.Matchers.arrayWithSize;
+import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.greaterThan;
@@ -8,9 +12,11 @@ import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThan;
 
 import java.util.Arrays;
 
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -100,6 +106,33 @@ public class UserJsonTest {
 			.body("age[1]", is(25))
 			.body("filhos.name", hasItem(Arrays.asList("Zezinho", "Luizinho")))
 			.body("salary", contains(1234.5677f, 2500, null))
+		;
+	}
+	
+	@Test
+	public void deveFazerVerificacoesAvancadas() {
+		given()
+		.when()
+			.get("https://restapi.wcaquino.me/users")
+		.then()
+			.statusCode(200)
+			.body("$", hasSize(3))
+			.body("age.findAll{it <= 25}.size()", is(2))
+			.body("age.findAll{it <= 25 && it > 20}.size()", is(1))
+			.body("findAll{it.age <= 25 && it.age > 20}.name", hasItem("Maria Joaquina"))
+			.body("findAll{it.age <= 25}[0].name", is("Maria Joaquina"))
+			.body("findAll{it.age <= 25}[-1].name", is("Ana Júlia"))
+			.body("find{it.age <= 25}.name", is("Maria Joaquina"))
+			.body("findAll{it.name.contains('n')}.name", hasItems("Maria Joaquina", "Ana Júlia"))
+			.body("findAll{it.name.length() > 10}.name", hasItems("João da Silva", "Maria Joaquina"))
+			.body("name.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+			.body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+			.body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}.toArray()", allOf(arrayContaining("MARIA JOAQUINA"), arrayWithSize(1)))
+			.body("age.collect{it * 2}", hasItems(60, 50, 40))
+			.body("id.max()", is(3))
+			.body("salary.min()", is(1234.5677f))
+			.body("salary.findAll{it != null}.sum()", is(closeTo(3734.5677f, 0.001)))
+			.body("salary.findAll{it != null}.sum()", allOf(greaterThan(3500d), lessThan(5000d)))
 		;
 	}
 }
