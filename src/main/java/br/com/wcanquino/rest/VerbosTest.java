2999e128e7719be96efcb0998d8523beca898b04
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-07-01 22:00:00 -0300

    Inserindo com POST

---
+++ src/main/java/br/com/wcanquino/rest/VerbosTest.java
@@ -0,0 +1,27 @@
+package br.com.wcanquino.rest;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+import org.junit.Test;
+
+public class VerbosTest {
+
+	@Test
+	public void deveSalvarUsuario() {
+		given()
+			.log().all()
+			.contentType("application/json")
+			.body("{\"name\":\"José\", \"age\":50}")
+		.when()
+			.post("https://restapi.wcaquino.me/users")
+		.then()
+			.log().all()
+			.statusCode(201)
+			.body("id", is(notNullValue()))
+			.body("name", is("José"))
+			.body("age", is(50))
+		;
+	}
+}
