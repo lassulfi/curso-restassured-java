563be8040c88854b09de6436ee5833a1307d93da
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-07-02 21:33:37 -0300

    Serializando objeto

--- src/main/java/br/com/wcanquino/rest/VerbosTest.java
+++ src/main/java/br/com/wcanquino/rest/VerbosTest.java
@@ -53,6 +53,25 @@ public class VerbosTest {
 	}
 	
 	@Test
+	public void deveSalvarUsuarioViaObjeto() {
+		User user = new User("Usuario via objeto", 35);
+		
+		given()
+			.log().all()
+			.contentType("application/json")
+			.body(user)
+		.when()
+			.post("https://restapi.wcaquino.me/users")
+		.then()
+			.log().all()
+			.statusCode(201)
+			.body("id", is(notNullValue()))
+			.body("name", is("Usuario via objeto"))
+			.body("age", is(35))
+		;
+	}
+	
+	@Test
 	public void deveSalvarUsuarioViaXML() {
 		given()
 			.log().all()
