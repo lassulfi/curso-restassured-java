fc15f0f416e74b0a76045979f44b353e26b00072
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-07-03 21:04:12 -0300

    Query parametrizável

--- src/main/java/br/com/wcanquino/rest/EnviosDadosTest.java
+++ src/main/java/br/com/wcanquino/rest/EnviosDadosTest.java
@@ -1,6 +1,7 @@
 package br.com.wcanquino.rest;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
 
 import org.junit.Test;
 
@@ -18,6 +19,21 @@ public class EnviosDadosTest {
 			.log().all()
 			.statusCode(200)
 			.contentType(ContentType.JSON)
+		;
+	}
+	
+	@Test
+	public void deveEnviarValorViaQueryViaParam() {
+		given()
+			.log().all()
+			.queryParam("format", "xml")
+		.when()
+			.get("https://restapi.wcaquino.me/v2/users")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.contentType(ContentType.XML)
+			.contentType(containsString("utf-8"))
 		;
 	}
 }
