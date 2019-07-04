af0bc7afb2538db669d3b5cd4e941b05629422fc
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-07-03 21:09:57 -0300

    Header

--- src/main/java/br/com/wcanquino/rest/EnviosDadosTest.java
+++ src/main/java/br/com/wcanquino/rest/EnviosDadosTest.java
@@ -36,4 +36,18 @@ public class EnviosDadosTest {
 			.contentType(containsString("utf-8"))
 		;
 	}
+	
+	@Test
+	public void deveEnviarValorViaHeader() {
+		given()
+			.log().all()
+			.accept(ContentType.XML)
+		.when()
+			.get("https://restapi.wcaquino.me/v2/users")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.contentType(ContentType.XML)
+		;
+	}
 }
