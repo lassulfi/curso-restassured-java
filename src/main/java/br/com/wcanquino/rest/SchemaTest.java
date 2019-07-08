aa375100354c6049f216d1b0c1f7475954a6347b
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-07-08 15:01:28 -0300

    Valindando JSON

--- src/main/java/br/com/wcanquino/rest/SchemaTest.java
+++ src/main/java/br/com/wcanquino/rest/SchemaTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.xml.sax.SAXParseException;
 
 import io.restassured.matcher.RestAssuredMatchers;
+import io.restassured.module.jsv.JsonSchemaValidator;
 
 public class SchemaTest {
 
@@ -34,4 +35,18 @@ public class SchemaTest {
 			.body(RestAssuredMatchers.matchesXsdInClasspath("users.xsd"))
 		;
 	}
+	
+	@Test
+	public void deveValidarSchemaJSON() {
+		given()
+			.log().all()
+		.when()
+			.get("http://restapi.wcaquino.me/users")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body(JsonSchemaValidator.matchesJsonSchemaInClasspath("users.json"))
+		;
+	}
+	
 }
