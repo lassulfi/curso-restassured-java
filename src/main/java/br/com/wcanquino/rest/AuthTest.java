f412e50b01c3aa56e9dc80bc60cd834462e71ed0
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-07-08 21:35:40 -0300

    Autenticacao Basica

--- src/main/java/br/com/wcanquino/rest/AuthTest.java
+++ src/main/java/br/com/wcanquino/rest/AuthTest.java
@@ -25,19 +25,72 @@ public class AuthTest {
 	@Test
 	public void deveObterClima() {
 		given()
-		.log().all()
-		.queryParam("q", "fortaleza,BR")
-		.queryParam("appid", "e1c682db71002e7e5a91fb775e4d6a67")
-		.queryParam("units", "metric")
-	.when()
-		.get("https://api.openweathermap.org/data/2.5/weather")
-	.then()
-		.log().all()
-		.statusCode(200)
-		.body("name", is("Fortaleza"))
-		.body("coord.lon", is(-38.52f))
-		.body("coord.lat", is(-3.73f))
-		.body("main.temp", Matchers.greaterThan(25.0f))
-	;
+			.log().all()
+			.queryParam("q", "fortaleza,BR")
+			.queryParam("appid", "e1c682db71002e7e5a91fb775e4d6a67")
+			.queryParam("units", "metric")
+		.when()
+			.get("https://api.openweathermap.org/data/2.5/weather")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("name", is("Fortaleza"))
+			.body("coord.lon", is(-38.52f))
+			.body("coord.lat", is(-3.73f))
+			.body("main.temp", Matchers.greaterThan(25.0f))
+		;
+	}
+	
+	@Test
+	public void naoDeveAcessarSemSenha() {
+		given()
+			.log().all()
+		.when()
+			.get("https://restapi.wcaquino.me/basicauth")
+		.then()
+			.log().all()
+			.statusCode(401)
+		;
+	}
+	
+	@Test
+	public void deveFazerAutenticacaoBasica() {
+		given()
+			.log().all()
+		.when()
+			.get("https://admin:senha@restapi.wcaquino.me/basicauth")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("status", is("logado"))
+		;
+	}
+	
+	@Test
+	public void deveFazerAutenticacaoBasica2() {
+		given()
+			.log().all()
+			.auth().basic("admin", "senha")
+		.when()
+			.get("https://restapi.wcaquino.me/basicauth")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("status", is("logado"))
+		;
+	}
+	
+	@Test
+	public void deveFazerAutenticacaoBasicaChallenge() {
+		given()
+			.log().all()
+			.auth().preemptive().basic("admin", "senha")
+		.when()
+			.get("https://restapi.wcaquino.me/basicauth2")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("status", is("logado"))
+		;
 	}
 }
